c190bf126466a5d5149b2100f2df191136a1e2d3
Kat  V <katpapacostas@gmail.com>
2020-02-05 17:30:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT  MAX(color) as prominent_color, COUNT(prominent_color)
+  FROM bears;
+"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
